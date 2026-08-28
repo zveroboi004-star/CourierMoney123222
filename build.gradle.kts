910d4bbe99ab4eded5b42c63a5a4b0eb0be17ddd
zveroboi004-star <zveroboi004@gmail.com>
2026-08-28 18:17:29 +0300

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    id("com.android.application") version "9.0.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
+}
